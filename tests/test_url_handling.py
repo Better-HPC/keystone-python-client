a41ab7a7ab48d19608d684a889ebd3cb36cfcd51
Daniel Perrefort <djperrefort@gmail.com>
2024-07-13 20:37:14 -0400

    Report test coverage to codacy (#24)

---
+++ tests/test_url_handling.py
@@ -0,0 +1,18 @@
+"""Tests for the handling to the base API url."""
+
+from unittest import TestCase
+
+from keystone_client import KeystoneClient
+
+
+class TestUrl(TestCase):
+    """Tests for the `url` property"""
+
+    def test_trailing_slash_removed(self):
+        """Test trailing slashes are removed from URLs provided at init"""
+
+        # Test for various numbers of trailing slashes
+        url = 'http://test.domain.com'
+        self.assertEqual(url, KeystoneClient(url).url)
+        self.assertEqual(url, KeystoneClient(url + '/').url)
+        self.assertEqual(url, KeystoneClient(url + '////').url)
