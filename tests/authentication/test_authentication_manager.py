e683f444ab1e5f802958cfeb7518462f9eabce01
djperrefort <djperrefort@gmail.com>
2024-07-20 21:02:01 -0400

    Adds tests for checking authentication status

--- tests/authentication/test_authentication_manager.py
+++ tests/authentication/test_authentication_manager.py
@@ -1,6 +1,6 @@
 """Tests for the `AuthenticationManager` class."""
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from unittest import TestCase
 
 import jwt
@@ -36,7 +36,34 @@ class IsAuthenticated(TestCase):
         self.assertFalse(manager.is_authenticated())
 
     def test_valid_jwt(self) -> None:
-        self.fail()
+        """Test the return value is `True` when the JWT token is not expired"""
+
+        manager = AuthenticationManager('auth', 'refresh', 'blacklist')
+        manager.jwt = self.create_token(
+            access_expires=datetime.now() + timedelta(hours=1),
+            refresh_expires=datetime.now() + timedelta(days=1)
+        )
+
+        self.assertTrue(manager.is_authenticated())
+
+    def test_refreshable_jwt(self) -> None:
+        """Test the return value is `True` when the JWT token expired but refreshable"""
+
+        manager = AuthenticationManager('auth', 'refresh', 'blacklist')
+        manager.jwt = self.create_token(
+            access_expires=datetime.now() - timedelta(hours=1),
+            refresh_expires=datetime.now() + timedelta(days=1)
+        )
+
+        self.assertTrue(manager.is_authenticated())
 
     def test_expired_jwt(self) -> None:
-        self.fail()
+        """Test the return value is `False` when the JWT token expired"""
+
+        manager = AuthenticationManager('auth', 'refresh', 'blacklist')
+        manager.jwt = self.create_token(
+            access_expires=datetime.now() - timedelta(days=1),
+            refresh_expires=datetime.now() - timedelta(hours=1)
+        )
+
+        self.assertFalse(manager.is_authenticated())
